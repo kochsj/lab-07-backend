51bdb542c3b455e19c09c682ed9e27b7049881d5
Stephen Koch <47271868+kochsj@users.noreply.github.com>
2019-11-06 15:41:38 -0800

    needed a darksky api key. weather is working now.

--- server.js
+++ server.js
@@ -23,25 +23,23 @@ server.use(errorHandler);
 ///////////////////////////////////////////////////////////////////////
 //Callback Functions
 ///////////////////////////////////////////////////////////////////////
-let tempArr;
+
 function locationHandler(req, res) {
   const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${req.query.data}&key=${process.env.GEOCODE_API_KEY}`;
   superagent.get(url).then(data => {
     let location = (new Location(req.query.data, data.body));
     res.status(200).send(location);
-    tempArr = (location);
   }).catch(error => errorHandler(error, req, res));
 }
 //Building a path to /weather
 function weatherHandler(req, res) {
-  console.log(tempArr);
-  const url = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${tempArr.latitude},${tempArr.longitude}`
+  const url = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${req.query.data.latitude},${req.query.data.longitude}`;
   superagent.get(url).then(data => {
-    let arr = data.daily.data.map(value => {
+    const weatherSum = data.body.daily.data.map(value => {
       return new Forecast(value);
     });
-    res.send(arr);
-  })
+    res.status(200).json(weatherSum);
+  }).catch(error => errorHandler(error, req, res));
 }
 
 function notFound(req, res) {
@@ -56,11 +54,15 @@ function errorHandler(error, req, res) {
 ///////////////////////////////////////////////////////////////////////
 //Constructor Functions
 ///////////////////////////////////////////////////////////////////////
-function Forecast(each) {
-  let temp = new Date((each.time) * 1000);
-  let tempScr = temp.toUTCString();
-  this.forecast = each.summary;
-  this.time = tempScr;
+// function Forecast(each) {
+//   let temp = new Date((each.time) * 1000);
+//   let tempScr = temp.toUTCString().slice(0, 15);
+//   this.forecast = each.summary;
+//   this.time = tempScr;
+// }
+function Forecast(day) {
+  this.forecast = day.summary;
+  this.time = new Date(day.time * 1000).toString().slice(0, 15);
 }
 
 function Location(city, geoData) {
